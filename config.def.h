fdb0cd16febe5409aa6379203e79d27c84d224fd
Yayu Wang <st.saint.wyy@gmail.com>
2022-11-25 10:28:26 -0800

    chore: remove dmenu

--- config.def.h
+++ config.def.h
@@ -12,7 +12,7 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
 static const char *fonts[]          = { "SauceCodePro Nerd Font Mono:size=12" };
-static const char dmenufont[]       = "monospace:size=10";
+/* static const char dmenufont[]       = "monospace:size=10"; */
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -62,11 +62,11 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char* dmenucmd[] = { "dmenu_run", "-m",  dmenumon,  "-fn",
-				  dmenufont,   "-nb", col_gray1, "-nf",
-				  col_gray3,   "-sb", col_cyan,  "-sf",
-				  col_gray4,   NULL };
+/* static char dmenumon[2] = "0"; /\* component of dmenucmd, manipulated in spawn() *\/ */
+/* static const char* dmenucmd[] = { "dmenu_run", "-m",  dmenumon,  "-fn", */
+/*				  dmenufont,   "-nb", col_gray1, "-nf", */
+/*				  col_gray3,   "-sb", col_cyan,  "-sf", */
+/*				  col_gray4,   NULL }; */
 static const char *roficmd[] = {"rofi", "-show", "run"};
 static const char *termcmd[]  = { "alacritty", NULL };
 
