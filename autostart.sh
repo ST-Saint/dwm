4a7c9cabf1c79bee2c654a51134bdab779b9fd17
Yayu Wang <st.saint.wyy@gmail.com>
2022-11-29 23:02:05 -0800

    feat: upgrade autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,11 +1,19 @@
-dunst &
-picom &
-volumeicon &
-ibus-daemon -x -d
-nitrogen --restore &
-flameshot &
-netease-cloud-music &
-WeChat &
-discord &
-LC_LANG=zh_cn emacs &
-google-chrome-stable &
+run_once () {
+    if ! pgrep -x ${1} > /dev/null
+    then
+        shift
+        sh -c '$@'
+    fi
+}
+
+run_once dunst dunst
+run_once picom picom
+run_once picom volumeicon
+run_once ibus-daemon ibus-daemon -x -d
+run_once nitrogen nitrogen --no-startup-id --restore
+run_once flameshot flameshot
+run_once netease-cloud-m netease-cloud-music
+run_once WeChat WeChat
+run_once discord discord
+run_once emacs LC_LANG=zh_cn emacs
+run_once chrome google-chrome-stable
