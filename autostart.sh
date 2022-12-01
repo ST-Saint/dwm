576efb2866820b4ecf2f6606b700f34e1ef0d173
Yayu Wang <st.saint.wyy@gmail.com>
2022-11-30 21:00:14 -0800

    fix: fix autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -2,13 +2,14 @@ run_once () {
     if ! pgrep -x ${1} > /dev/null
     then
         shift
-        sh -c '$@'
+        $@ &
     fi
 }
 
+run_once dwmblocks $HOME/.dwm/dwmblocks/dwmblocks
 run_once dunst dunst
 run_once picom picom
-run_once picom volumeicon
+run_once volumicon volumeicon
 run_once ibus-daemon ibus-daemon -x -d
 run_once nitrogen nitrogen --no-startup-id --restore
 run_once flameshot flameshot
